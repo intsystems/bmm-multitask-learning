efb98a8ffed025984ba83de8d68c11cfbc8a7092
sem-k32 <kirill.semkin32@mail.ru>
2025-03-30 16:21:04 +0300

    Added Predictive distribution class

--- bmm_multitask_learning/variational/distr.py
+++ bmm_multitask_learning/variational/distr.py
@@ -1,6 +1,8 @@
 """Utils for working with distributions
 """
 from typing import Callable
+from itertools import product
+from pipe import select
 
 import torch
 from torch import nn
@@ -25,5 +27,43 @@ def kl_sample_estimation(
     return (log_p_1 - log_p_2).mean()
 
 
-class Predictive():
-    ...
+class Predictive(distr.Distribution):
+    def __init__(
+        self,
+        task_distr: TargetDistr,
+        classifier_distr: distr.Distribution,
+        latent_distr: LatentDistr,
+        x: torch.Tensor,
+        classifier_num_particles: int = 1,
+        latent_num_particles: int = 1,
+    ):
+        # sample hidden state (classifier + latent) from posterior
+        classifier_samples = classifier_distr.sample((classifier_num_particles, ))
+        latent_samples = latent_distr(x).sample((latent_num_particles, ))
+        # build conditional distribution objects for target
+        self.target_distrs = list(
+            product(classifier_samples, latent_samples) |
+            select(lambda cl_lt: task_distr(*cl_lt))
+        )
+
+        target_distr_example: distr.Distribution = self.target_distrs[0]
+        super().__init__(
+            batch_shape=target_distr_example.batch_shape, 
+            event_shape=target_distr_example.event_shape, 
+            validate_args=target_distr_example._validate_args
+        )
+
+    def log_prob(self, value):
+        return torch.concat(list(
+            self.target_distrs | select(lambda distr: distr.log_prob(value))
+        )).mean()
+
+    def sample(self, sample_shape = ...):
+        # choose distr from mix
+        indx = torch.randint(0, len(self.target_distrs), sample_shape).flatten().tolist()
+        # sample from chosen distr
+        samples = torch.concat(list(indx | select(lambda i: self.target_distrs[i].sample())))
+        # tune shape for the output samples
+        samples = samples.reshape(*sample_shape, -1)
+
+        return samples
