2109589f6c97ce9544e65d8420f68084622779da
sem-k32 <kirill.semkin32@mail.ru>
2025-04-14 00:07:50 +0300

    Extracted common params in data generation

--- examples/variational/elementary/data.py
+++ examples/variational/elementary/data.py
@@ -7,23 +7,26 @@ from torch.utils.data import TensorDataset
 from torch.distributions import Normal, Bernoulli
 
 
+# TODO: may be change sharing params for linked and solo dataset later
+SIGMA_X = 5.
+SIGMA_FACTOR = 1.5
+
+
 def build_linked_datasets(
     size: int,
     dim: int = 2
 ) -> tuple[TensorDataset]:
-    SIGMA_X = 5.
     EPS_X = 1e-2
 
     X = SIGMA_X * torch.randn((size, dim))
     X1 = X + EPS_X * torch.randn_like(X)
     X2 = X + EPS_X * torch.randn_like(X)
-
-    SIGMA_FACTOR = 1.5
+   
     EPS_W = 1.
 
     SIGMA_W = SIGMA_FACTOR * torch.ones((dim, ))
-    SIGMA_W1 = SIGMA_W + EPS_W * (torch.rand_like(SIGMA_W) - 0.5)
-    SIGMA_W2 = SIGMA_W + EPS_W * (torch.rand_like(SIGMA_W) - 0.5)
+    SIGMA_W1 = SIGMA_W + EPS_W * (torch.rand_like(SIGMA_W) - 0.5)   # Uniform[-0.5, 0.5]
+    SIGMA_W2 = SIGMA_W + EPS_W * (torch.rand_like(SIGMA_W) - 0.5)   # Uniform[-0.5, 0.5]
     w1 = Normal(X2.mean(dim=0), SIGMA_W1).sample()
     w2 = Normal(X1.mean(dim=0), SIGMA_W2).sample()
 
@@ -37,11 +40,7 @@ def build_solo_dataset(
     size: int,
     dim: int = 2
 ) -> TensorDataset:
-    SIGMA_X = 5.0
-
     X = SIGMA_X * torch.randn((size, dim))
-
-    SIGMA_FACTOR = 1.5
 
     SIGMA_W = SIGMA_FACTOR * torch.ones((dim, ))
     # prior for w does not depend on any X
